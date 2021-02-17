141d33366067b7edc2e7156edc0caccd5d0c6d33
Marcus Olsson <marcus.olsson@hey.com>
2021-02-17 10:53:17 +0100

    Bump version

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -1,4 +1,8 @@
-export { standardOptionsCompat, hasCapability } from "./capability";
+export {
+  standardOptionsCompat,
+  fieldConfigWithMinMaxCompat,
+  hasCapability,
+} from "./capability";
 export { toTimeField } from "./fields";
 export { getFormattedDisplayValue, measureText } from "./format";
 export { getPanelPluginOrFallback } from "./dependency";
